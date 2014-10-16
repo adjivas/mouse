a1c89764acb566a2cb52980b0f6578a8e377026c
adjivas <adjivas@student.42.fr>
2014-10-16 10:48:20 +0200

    simple save with LICENCE

--- app/script/mouscreen/configuration.js
+++ app/script/mouscreen/configuration.js
@@ -32,14 +32,14 @@ var Configuration = {
 
     Configuration.active = !Configuration.active;
     gui.Window.open(url, wid).on('close', Configuration.end);
-    Configuration.win.hide();
+//    Configuration.win.hide();
   },
   'end': function (arg) {
     Configuration.active = !Configuration.active;
     this.close(true);
     Door.close();
     Configuration.win.reload();
-    Configuration.win.show(true);
+//    Configuration.win.show(true);
   },
   'default': window.addEventListener('mouseup', function(arg) {
     var elemt = arg.toElement;
